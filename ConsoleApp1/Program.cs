9ac98979100c3ffedeee41644581854568a8399f
ibrahim <ibrahimkrgluu@gmail.com>
2024-10-03 19:16:39 +0300

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for(var i = 0 ; i<nums.Length  ; i++ ){
+            for(var j =1 ; j<nums.Length ; j++ )
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+            
+                }
+        }
+        
+        return[];
+      
+    }
+}
